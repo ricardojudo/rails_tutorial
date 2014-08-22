393d087e703c9932332f88576ea761ad2fc610b6
Ricardo Castañeda <ricardojudo@gmail.com>
2014-08-21 22:08:25 -0500

    Carrierwave enabled with local storage

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822022752) do
+ActiveRecord::Schema.define(version: 20140822024808) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
 end
