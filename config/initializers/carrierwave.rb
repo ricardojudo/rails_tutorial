c06f9d63590b7ade9606853629f1bbc1aa872450
Ricardo Castañeda <ricardojudo@gmail.com>
2014-08-23 23:29:09 -0500

    Set aws settings for carrierwave as env variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
  
     # Configuration for Amazon S3
     :provider              => 'AWS',
-    :aws_access_key_id     => 'AKIAJOEBOJCCGVKEH2AA',#ENV['S3_KEY'],
+    :aws_access_key_id     => ENV['S3_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET'],
     :region                => ENV['S3_REGION']
   }
